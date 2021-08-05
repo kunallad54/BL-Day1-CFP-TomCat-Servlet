bcd4050bd321ec2b7633e396a5fea625c496513b
kunallad54 <kunallad5473@gmail.com>
2021-08-05 12:59:06 +0530

    Creating First Servlet

---
+++ FirstServletProject/src/main/java/com/brigelabz/krunal/FirstServlet.java
@@ -0,0 +1,24 @@
+package com.brigelabz.krunal;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/html");
+		PrintWriter out  = resp.getWriter();
+		out.println("<h3> Hello Krunal !!! This is my First Servlet .</h3>");
+		out.close();
+	}
+		
+}
